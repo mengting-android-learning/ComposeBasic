d68903eadcaa0fe1109687202448bb4698ff228e
Rachel-oc <mengting.xie@thoughtworks.com>
2023-03-21 09:27:13 +0800

    feat:state

--- app/src/main/java/com/example/composebasic/MainActivity.kt
+++ app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -17,12 +17,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBasicTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
---
+++ app/src/main/java/com/example/composebasic/WellnessTask.kt
@@ -0,0 +1,3 @@
+package com.example.composebasic
+
+data class WellnessTask(val id: Int, val label: String)
